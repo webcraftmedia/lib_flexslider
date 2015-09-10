9e05e239c10de69706447dfa2d8c95aa20b193f2
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2015-09-10 04:33:10 +0200

    flexislider init

---
+++ lib_flexslider.php
@@ -0,0 +1,12 @@
+<?php
+namespace LIB;
+class lib_flexslider extends \LIB\lib_jscss{
+    public static function get_class(){
+        return self::class;}
+    public static function js_path(){
+        return \SYSTEM\WEBPATH(new \PLIB(),'flexslider/lib/jquery.flexslider-min.js');}
+    public static function css_path(){
+        return \SYSTEM\WEBPATH(new \PLIB(),'flexslider/lib/flexslider.less');}
+    public static function version(){
+        return '<a href="http://flexslider.woothemes.com/">2</a>';}
+}
