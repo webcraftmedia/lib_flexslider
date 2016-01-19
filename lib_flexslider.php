c25a1106e6867bc3d738eeb2d9b908f85ffa5769
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-01-19 21:54:07 +0100

    new caching system implemented, updated system reference

--- lib_flexslider.php
+++ lib_flexslider.php
@@ -4,9 +4,9 @@ class lib_flexslider extends \LIB\lib_jscss{
     public static function get_class(){
         return self::class;}
     public static function js_path(){
-        return \SYSTEM\WEBPATH(new \PLIB(),'flexslider/lib/jquery.flexslider-min.js');}
+        return new \PLIB('flexslider/lib/jquery.flexslider-min.js');}
     public static function css_path(){
-        return \SYSTEM\WEBPATH(new \PLIB(),'flexslider/lib/flexslider.less');}
+        return new \PLIB('flexslider/lib/flexslider.less');}
     public static function version(){
         return '<a href="http://flexslider.woothemes.com/">2</a>';}
 }
