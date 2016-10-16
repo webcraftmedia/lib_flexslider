f8f0ff421686a4e46d571b0bc8cdcf5310c7954d
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-10-16 16:42:01 +0200

    fixed flexslider css include

--- lib_flexslider.php
+++ lib_flexslider.php
@@ -6,7 +6,7 @@ class lib_flexslider extends \LIB\lib_jscss{
     public static function js_path(){
         return new \PLIB('flexslider/lib/jquery.flexslider-min.js');}
     public static function css_path(){
-        return new \PLIB('flexslider/lib/flexslider.less');}
+        return new \PLIB('flexslider/lib/flexslider.css');}
     public static function version(){
         return '<a href="http://flexslider.woothemes.com/">2</a>';}
 }
